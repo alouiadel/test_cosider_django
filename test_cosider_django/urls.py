530d6dcf1187d4ba48d07137ece805b0291ca513
Abdelaali Adel Aloui <alouiadel@tuta.io>
2024-03-02 21:04:31 +0100

    Added a custom 404 handler and defined urlpatterns

--- test_cosider_django/urls.py
+++ test_cosider_django/urls.py
@@ -1,22 +1,19 @@
-"""
-URL configuration for test_cosider_django project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+from cosider import views
+
+handler404 = 'cosider.views.custom_404_view'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Redirect the root URL to the 'invoices' page
+    path('', RedirectView.as_view(url='invoices/', permanent=True)),
+
+    # Read and display invoice data
+    path('invoices/', views.display_invoices, name='display_invoices'),
+
+    # Search by item label
+    path('search/', views.search_invoices, name='search_invoices'),
+
+    # Print invoice
+    path('print/', views.print_invoice, name='print_invoice'),
 ]
